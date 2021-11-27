f396d2b84e0823e289a2ae2dca55c4c6d48e0a33
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-28 01:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     with user_client:
         while True:
             print("[INFO] starting to check uptime..")
-            edit_text = f"🔰𝐓𝐫𝐚𝐜𝐤𝐬𝐭𝐮𝐝𝐢𝐨'𝐬 𝐁𝐨𝐭𝐬 𝐒𝐭𝐚𝐭𝐮𝐬🔰\n\n__( All bots are checked automatically if any correction report it )__\n\n\n"
+            edit_text = f"🤖 𝙋𝙔𝙍𝙊𝙂𝙍𝘼𝙈𝙈𝙀𝙍𝙎 𝘽𝙤𝙩 𝙎𝙩𝙖𝙩𝙪𝙨 🤖\n\n__( 𝐴𝑙𝑙 𝑏𝑜𝑡𝑠 𝑠𝑡𝑎𝑡𝑢𝑠 𝑎𝑟𝑒 𝑐ℎ𝑒𝑐𝑘𝑒𝑑 𝑎𝑢𝑡𝑜𝑚𝑎𝑡𝑖𝑐𝑎𝑙𝑙𝑦 𝑒𝑣𝑒𝑟𝑦 𝑓𝑜𝑢𝑟 ℎ𝑜𝑢𝑟𝑠. 𝐼𝑓 𝑎𝑛𝑦 𝑖𝑠𝑠𝑢𝑒 𝑝𝑙𝑒𝑎𝑠𝑒 𝑟𝑒𝑝𝑜𝑟𝑡 𝑖𝑡. )__\n\n\n"
             for bot in bots:
                 print(f"[INFO] checking @{bot}")
                 snt = user_client.send_message(bot, '/start')
@@ -31,12 +31,12 @@ def main():
                 msg = user_client.get_history(bot, 1)[0]
                 if snt.message_id == msg.message_id:
                     print(f"[WARNING] @{bot} is down")
-                    edit_text += f"𝙱𝙾𝚃 𝙽𝙰𝙼𝙴    {bot} \n𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @{bot}\n𝚂𝚃𝙰𝚃𝚄𝚂 ❌\n\n"
+                    edit_text += f"𝗕𝗼𝘁 𝗡𝗮𝗺𝗲    {bot} \n𝗨𝘀𝗲𝗿𝗡𝗮𝗺𝗲  @{bot}\n𝗦𝘁𝗮𝘁𝘂𝘀      ❌\n\n"
                     #user_client.send_message("me",
                                              #f"@{bot} was down")
                 else:
                     print(f"[INFO] all good with @{bot}")
-                    edit_text += f"𝙱𝙾𝚃 𝙽𝙰𝙼𝙴    {bot} \n𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @{bot}\n𝚂𝚃𝙰𝚃𝚄𝚂 ✅\n\n"
+                    edit_text += f"𝗕𝗼𝘁 𝗡𝗮𝗺𝗲    {bot} \n𝗨𝘀𝗲𝗿𝗡𝗮𝗺𝗲  @{bot}\n𝗦𝘁𝗮𝘁𝘂𝘀      ✅\n\n"
                 user_client.read_history(bot)
 
             time_now = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
